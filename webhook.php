0bf1a2f432b1a619dbc00b01c7ca5e6e377f178b
samwert452 <samwert200@gmail.com>
2025-07-18 20:49:43 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -8,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data = json_decode(file_get_contents('php://input'), true);
    
     $dsn = "pgsql:host=dpg-d1t8690d13ps7396dnj0-a.oregon-postgres.render.com;port=5432;dbname=gitivity;sslmode=require";
-    $username = "gitivity_user";
+    $username = "emails";
     $password = $_ENV['DB_PASSWORD'];
    
     try {
